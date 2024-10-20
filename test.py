6ca1a5733f8d4fa7ea43657672e8e55f11df82d6
vivianpinto85 <vivianpinto85@yahoo.com>
2024-10-20 12:52:09 +0700

    initna

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Create a small DataFrame
-data = {'Name': ['Alice', 'Bob', 'Charlie'],
+data = {'Name': ['Alice', 'Bob', 'Charlide'],
         'Age': [25, 30, 35],
         'City': ['New York', 'Los Angeles', 'Chicago']}
 
@@ -9,4 +9,5 @@ df = pd.DataFrame(data)
 
 # Display the DataFrame
 print("DataFrame:")
+
 print(df)
